698989acdb2310d4966bc75e62a7982cf98fda20
Harlhey Wider Grammont <widergrammont@gmail.com>
2022-04-17 18:02:30 -0400

    commit tree a4c60de13999a37c08e4ef5bffc08c90a2ce5cbc

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(array, event) {
+    const newArray = [];
+    for (let i=0; i<3; i++){
+        newArray.push (`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+        return newArray
+
+}
+
+function countDown() {
+let count = 10
+while (count >= 0) {
+    console.log(count--)
+
+}
+
+}
